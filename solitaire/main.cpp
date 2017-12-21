238e4d1207bbe17cce1f3dd3fa6905bc64d77f02
yuda <yuda@tebengan.id>
2017-12-21 08:14:43 +0700

    Initial Commit

---
+++ solitaire/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  solitaire
+//
+//  Created by yuda on 21/12/17.
+//  Copyright © 2017 yuda prama. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
